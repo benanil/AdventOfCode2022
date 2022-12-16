05208128a14e567da686a1f6bcd27ac3df0d8764
Anılcan Gülkaya <48527900+benanil@users.noreply.github.com>
2022-12-16 14:03:01 +0300

    Update and rename Vector2.hpp to Math.hpp

--- Math.hpp
+++ Math.hpp
@@ -1,6 +1,34 @@
 #pragma once
 #include <cmath>
 
+constexpr float PI = 3.14159265f;
+constexpr float RadToDeg = 180.0f / PI;
+constexpr float DegToRad = PI / 180.0f;
+constexpr float OneDivPI = 1.0f / PI;
+constexpr float PIDiv2 = PI / 2.0f;
+constexpr float TwoPI = PI * 2.0f;
+
+template<typename T> inline T Max(const T a, const T b) { return a > b ? a : b; }
+template<typename T> inline T Min(const T a, const T b) { return a < b ? a : b; }
+template<typename T> inline T Clamp(const T x, const T a, const T b) { return Max(a, Min(b, x)); }
+
+template<typename RealT>
+inline RealT Lerp(const RealT from, const RealT to, const RealT t) noexcept {
+	return from + (to - from) * t;
+}
+
+inline float rsqrt(float number) {
+	long i;
+	float x2, y;
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( 1.5f - ( x2 * y * y ) );   // 1st iteration
+	return y;
+}
+
 template<typename T>
 struct Vector2
 {
@@ -9,7 +37,7 @@ struct Vector2
 	Vector2()                   noexcept  : x(0), y(0) { }
 	constexpr Vector2(T a, T b) noexcept  : x(a), y(b) { }
 	
-	float Length()		  const { return sqrtf(LengthSquared()); }
+	float Length()	      const { return sqrtf(LengthSquared()); }
 	float LengthSquared() const { return x * x + y * y; }
 
 	static float Distance(const Vector2<T> a, const Vector2<T> b) {
@@ -17,7 +45,7 @@ struct Vector2
 		float diffy = a.y - b.y;
 		return sqrtf(diffx * diffx + diffy * diffy);
 	}
-    // square distance
+        // distance squared
 	static float DistanceSq(const Vector2<T> a, const Vector2<T> b) {
 		float diffx = a.x - b.x;
 		float diffy = a.y - b.y;
@@ -52,6 +80,11 @@ struct Vector2
 	bool operator == (Vector2 b) const { return x == b.x && y == b.y; }
 	bool operator != (Vector2 b) const { return x != b.x || y != b.y; }
 	bool operator <  (Vector2 b) const { return x < b.x && y < b.y; }
+	
+	static constexpr Vector2 Up()       { return Vector2( 0,  1); } 
+	static constexpr Vector2 Left()     { return Vector2(-1,  0); } 
+	static constexpr Vector2 Down()     { return Vector2( 0, -1); } 
+	static constexpr Vector2 Right()    { return Vector2( 1,  0); } 
 };
 
 using Vector2f = Vector2<float>;
