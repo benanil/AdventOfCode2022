cd8f7b637028173f7722a93cfa21aa046610c512
Anılcan Gülkaya <48527900+benanil@users.noreply.github.com>
2022-12-21 21:20:40 +0300

    Update Math.hpp

--- Math.hpp
+++ Math.hpp
@@ -100,14 +100,118 @@ struct Vector2
 };
 
 template<typename T>
+struct Vector3
+{
+	union
+	{
+		struct { T x, y, z; };
+		T arr[3];
+	};
+
+	static constexpr int NumElements = 3;
+	using ElemType = T;
+
+	Vector3() : x(0), y(0), z(0) { }
+	constexpr Vector3(T scale) : x(scale), y(scale), z(scale) { }
+	constexpr Vector3(T a, T b, T c) : x(a), y(b), z(c) { }
+
+	T& operator[] (int index) { return arr[index]; }
+	T operator[] (int index) const { return arr[index]; }
+
+	float Length() const { return sqrtf(LengthSquared()); }
+	constexpr float LengthSquared() const { return x * x + y * y + z * z; }
+
+	Vector3& Normalized() { *this /= Length(); return *this; }
+
+	static float Distance(const Vector3& a, const Vector3& b)
+	{
+		Vector3 diff = a - b; diff *= diff;
+		return sqrtf(diff.x + diff.y + diff.z);
+	}
+
+	// distance squared for comparing distances, faster than distance 
+	static float DistanceSq(const Vector3& a, const Vector3& b)
+	{
+		Vector3 diff = a - b; diff *= diff;
+		return diff.x + diff.y + diff.z;
+	}
+
+	static float Length(const Vector3& vec) { return vec.Length(); }
+
+	static float Dot(const Vector3& a, const Vector3& b)
+	{
+		return a.x * b.x + a.y * b.y + a.z * b.z;
+	}
+
+	static Vector3 Lerp(const Vector3& a, const Vector3& b, float t)
+	{
+		return Vector3(
+			a.x + (b.x - a.x) * t,
+			a.y + (b.y - a.y) * t,
+			a.z + (b.z - a.z) * t
+		);
+	}
+
+	static Vector3 Cross(const Vector3& a, const Vector3& b)
+	{
+		return Vector3(a.y * b.z - b.y * a.z,
+			a.z * b.x - b.z * a.x,
+			a.x * b.y - b.x * a.y);
+	}
+
+	static Vector3 Reflect(const Vector3& in, const Vector3& normal)
+	{
+		return in - normal * Dot(normal, in) * 2.0f;
+	}
+	// for more accuracy you can use sqrt instead of rsqrt: a / sqrtf(dot(a,a)), with double percison use sqrt instead of sqrtf   
+	static Vector3 Normalize(const Vector3& a) {
+		return a * rsqrt(Dot(a, a));
+	}
+
+	Vector3 operator-() { return Vector3f(-x, -y, -z); }
+	Vector3 operator+(const Vector3& other) const { return Vector3(x + other.x, y + other.y, z + other.z); }
+	Vector3 operator*(const Vector3& other) const { return Vector3(x * other.x, y * other.y, z * other.z); }
+	Vector3 operator/(const Vector3& other) const { return Vector3(x / other.x, y / other.y, z / other.z); }
+	Vector3 operator-(const Vector3& other) const { return Vector3(x - other.x, y - other.y, z - other.z); }
+
+	Vector3 operator+(T other) const { return Vector3(x + other, y + other, z + other); }
+	Vector3 operator*(T other) const { return Vector3(x * other, y * other, z * other); }
+	Vector3 operator/(T other) const { return Vector3(x / other, y / other, z / other); }
+	Vector3 operator-(T other) const { return Vector3(x - other, y - other, z - other); }
+
+	Vector3 operator += (const Vector3& o) { x += o.x; y += o.y; z += o.z; return *this; }
+	Vector3 operator *= (const Vector3& o) { x *= o.x; y *= o.y; z *= o.z; return *this; }
+	Vector3 operator /= (const Vector3& o) { x /= o.x; y /= o.y; z /= o.z; return *this; }
+	Vector3 operator -= (const Vector3& o) { x -= o.x; y -= o.y; z -= o.z; return *this; }
+
+	Vector3 operator += (T o) { x += o; y += o; z += o; return *this; }
+	Vector3 operator *= (T o) { x *= o; y *= o; z *= o; return *this; }
+	Vector3 operator /= (T o) { x /= o; y /= o; z /= o; return *this; }
+	Vector3 operator -= (T o) { x -= o; y -= o; z -= o; return *this; }
+
+	static constexpr Vector3 Zero()    { return Vector3(0.0, 0.0, 0.0); }
+	static constexpr Vector3 One()     { return Vector3(1.0, 1.0, 1.0); }
+	static constexpr Vector3 Up()      { return Vector3(0.0, 1.0, 0.0); }
+	static constexpr Vector3 Left()    { return Vector3(-1.0, 0.0, 0.0); }
+	static constexpr Vector3 Down()    { return Vector3(0.0, -1.0, 0.0); }
+	static constexpr Vector3 Right()   { return Vector3(1.0, 0.0, 0.0); }
+	static constexpr Vector3 Forward() { return Vector3(0.0, 0.0, 1.0); }
+	static constexpr Vector3 Backward(){ return Vector3(0.0, 0.0, -1.0); }
+};
+
+template<typename T>
 inline Vector2<T> MinT(const Vector2<T>& a, const Vector2<T>& b) { return Vector2<T>(Min(a.x, b.x), Min(a.y, b.y)); }
 template<typename T>
 inline Vector2<T> MaxT(const Vector2<T>& a, const Vector2<T>& b) { return Vector2<T>(Max(a.x, b.x), Max(a.y, b.y)); }
 
+using Vector2d = Vector2<double>;
 using Vector2f = Vector2<float>;
 using Vector2i = Vector2<int>;
 using Vector2s = Vector2<short>;
+using Vector2c = Vector2<char>;
 
-using Vector2f = Vector2<float>;
-using Vector2i = Vector2<int>;
-using Vector2s = Vector2<short>;
+using Vector3d = Vector3<double>;
+using Vector3f = Vector3<float>;
+using Vector3i = Vector3<int>;
+using Vector3s = Vector3<short>;
+using Vector3c = Vector3<char>;
